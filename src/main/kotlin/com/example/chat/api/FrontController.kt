747221791bd4ab8462323a43a41a4b8775b7f7d6
jo <whwo1214@gmail.com>
2024-07-08 14:50:35 +0900

    채팅룸1

--- src/main/kotlin/com/example/chat/api/FrontController.kt
+++ src/main/kotlin/com/example/chat/api/FrontController.kt
@@ -1,12 +1,22 @@
 package com.example.chat.api
 
+import com.example.chat.service.RoomService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 @CrossOrigin(origins = ["*"])
-class FrontController {
+class FrontController(private val roomService: RoomService) {
+
+    @GetMapping("/")
+    fun mainPage(model: Model): String {
+        val rooms = roomService.getAllRooms()
+        model.addAttribute("rooms", rooms)
+        return "main"
+    }
+
 
     @GetMapping("/chats")
     fun getChatPage(): String {
---
+++ src/main/kotlin/com/example/chat/service/RoomService.kt
@@ -0,0 +1,24 @@
+package com.example.chat.service
+
+import com.example.chat.data.Room
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class RoomService {
+    private val rooms: MutableMap<UUID, Room> = mutableMapOf()
+
+    fun getAllRooms(): List<Room> {
+        return rooms.values.toList()
+    }
+
+    fun createRoom(name: String): UUID {
+        val roomId = UUID.randomUUID()
+        rooms[roomId] = Room(roomId, name)
+        return roomId
+    }
+
+    fun getRoomById(roomId: UUID): Room? {
+        return rooms[roomId]
+    }
+}
